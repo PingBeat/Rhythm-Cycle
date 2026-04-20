40e07cb948f7eb121359121f56da6162a499e7f1
Lily AUBERT <lily.aubert69@gmail.com>
2026-04-20 15:25:30 +0200

    Commentaire conductor.gd

--- Gameplay/conductor.gd
+++ Gameplay/conductor.gd
@@ -1,49 +1,48 @@
-extends AudioStreamPlayer
+extends AudioStreamPlayer #Noeud Godot pour jouer des sons 2D
+
+#Déclaration de variables
 @onready var start_timer: Timer = $StartTimer
+@export var songBPM: int #Tempo de la musique
+var beatBeforeStart: int #Battement avant début de la musique
+var secPerBeat: float #Durée d'un temps en secondes
+var songPosition: float #Position de lecture en seconde
+var songPositioninBeats := 0 #Position de lecture en battement
+var prevSongPositioninBeats := 0 #Position précédente de lecture en battement
+signal beatSignal(position) #Crée un signal avec la position de lecture
 
-@export var songBPM: int
-var beatBeforeStart: int
-
-var secPerBeat: float
-
-var songPosition: float
-
-var songPositioninBeats := 0
-var prevSongPositioninBeats := 0 
-
-signal beatSignal(position)
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	secPerBeat = 60.0/songBPM
+	secPerBeat = 60.0/songBPM #Calcule la durée d'un battement
+	#Niveau 1 = 80BPM, Niveau 2 = 120BPM, Niveau 3 = 144BPM
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
+	#Appelée 60 fois par seconde pour suivre la musique avec précision
 	if playing:
-		songPosition = get_playback_position() + AudioServer.get_time_since_last_mix()
-		songPosition -= AudioServer.get_output_latency()
-		songPositioninBeats = int(floor(songPosition / secPerBeat)) + beatBeforeStart
-		report_beat()
+		songPosition = get_playback_position() + AudioServer.get_time_since_last_mix() #Calcule la position
+		songPosition -= AudioServer.get_output_latency() #en tenant compte de la latence
+		songPositioninBeats = int(floor(songPosition / secPerBeat)) + beatBeforeStart #Convertit en battement
+		report_beat() #Emet un signal qui donne le battement actuel
 		
 func report_beat():
-	if (prevSongPositioninBeats < songPositioninBeats):
-		emit_signal("beatSignal",songPositioninBeats)
-		prevSongPositioninBeats = songPositioninBeats
+	#Emet un signal qui donne le battement actuel et fait apparaitre une note
+	if (prevSongPositioninBeats < songPositioninBeats): #Si la position dans le son change
+		emit_signal("beatSignal",songPositioninBeats) #Emet un signal
+		prevSongPositioninBeats = songPositioninBeats #Actualise la position
 		
 func playWithBeatOffset(beats):
-	beatBeforeStart = beats
-	start_timer.wait_time = secPerBeat
-	start_timer.start()
+	#Démarre la musique avec un décalage pour laisser les notes descendre avant que la musique ne commence
+	beatBeforeStart = beats #Nombre de battements avant de commencer la musique
+	start_timer.wait_time = secPerBeat #Compte un certains nombres de battements avant de commencer la musique
+	start_timer.start() #Démarre le timer du temps d'attente
 	
 func _on_start_timer_timeout() -> void:
+	#Gère le décompte en temps réel
 	songPositioninBeats += 1
-	if songPositioninBeats < beatBeforeStart - 1 :
-		start_timer.start()
-	elif songPositioninBeats == beatBeforeStart - 1:
-		start_timer.wait_time = start_timer.wait_time - (AudioServer.get_time_to_next_mix() + AudioServer.get_output_latency())
-		start_timer.start()
-	else:
+	if songPositioninBeats < beatBeforeStart - 1 : #Si la musique n'a pas encore commencée
+		start_timer.start() #Lance le timer 
+	elif songPositioninBeats == beatBeforeStart - 1: #Si on est juste avant le début de la musique
+		start_timer.wait_time = start_timer.wait_time - (AudioServer.get_time_to_next_mix() + AudioServer.get_output_latency()) #On prend en compte la latence
+		start_timer.start() #On relance le timer
+	else: #Sinon on peut commencer la musique
 		play()
-		start_timer.stop()
-	report_beat()
+		start_timer.stop() #Stop le timer
+	report_beat() #Emet un signal qui donne le battement actuel
