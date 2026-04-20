44b9b7b3606618fcb71a483cc2f8544055dcbe9c
Lily AUBERT <lily.aubert69@gmail.com>
2026-04-20 14:01:34 +0200

    Commentaires ui_sound_manager.gd

--- Autoloads/ui_sound_manager.gd
+++ Autoloads/ui_sound_manager.gd
@@ -1,38 +1,32 @@
-extends Node
-
-var click_player: AudioStreamPlayer
+extends Node #Classe de base Node de Godot
+var click_player: AudioStreamPlayer #Noeud Godot pour jouer des sons 2D
 
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS # Pour que le son marche même quand le jeu est en pause
-	click_player = AudioStreamPlayer.new()
-
-	# preload() est obligatoire ici, sinon Godot n'emballe pas le fichier dans le .exe
-	click_player.stream = preload("res://Assets/Audio/click.wav")
-		
-	add_child(click_player)
-	
-	get_tree().node_added.connect(_on_node_added)
-	
-	_connect_buttons(get_tree().root)
+	click_player = AudioStreamPlayer.new() #Crée le nœud de lecture audio
+	click_player.stream = preload("res://Assets/Audio/click.wav") #charge le fichier audio à la compilation
+	add_child(click_player) #Attache le lecteur audio au noeud
+	get_tree().node_added.connect(_on_node_added) #Connecte les boutons créées dynamiquement
+	_connect_buttons(get_tree().root) #Connecte les boutons déjà créées
 
 func _on_node_added(node: Node) -> void:
-	if node is BaseButton:
+	#Detecte quand un noeud est ajouté
+	if node is BaseButton: #Si c'est un bouton
 		if not node.pressed.is_connected(_on_button_pressed):
-			node.pressed.connect(_on_button_pressed)
+			node.pressed.connect(_on_button_pressed) #Connecte le son lié si ce n'est pas déjà fait
 
 func _connect_buttons(node: Node) -> void:
-	if node is BaseButton:
+	#Fonction récursive qui parcours les noeuds et connecte les boutons
+	if node is BaseButton: #Si le noeud est un bouton
 		if not node.pressed.is_connected(_on_button_pressed):
-			node.pressed.connect(_on_button_pressed)
-			
-	for child in node.get_children():
+			node.pressed.connect(_on_button_pressed) #Connecte le son lié si ce n'est pas déjà fait
+	for child in node.get_children(): #Parcours les noeuds enfants
 		_connect_buttons(child)
 
 func _on_button_pressed() -> void:
-	# Dès qu'on clique, on lance le bruit
+	#Joue le son de clic de manière sécurisée
 	if click_player.stream != null:
-		# On copie le lecteur au cas où on clique super vite plusieurs fois
-		var duplicate_player = click_player.duplicate()
-		add_child(duplicate_player)
-		duplicate_player.play()
-		duplicate_player.finished.connect(duplicate_player.queue_free)
+		var duplicate_player = click_player.duplicate()#On copie le lecteur au cas où on clique vite plusieurs fois
+		add_child(duplicate_player) #On crée le noeud
+		duplicate_player.play() #On joue le son
+		duplicate_player.finished.connect(duplicate_player.queue_free) #On libère la fil
